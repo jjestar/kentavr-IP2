0b3b4ff73cb827bdb63c435cd9810d366e51a218
dias(jjes) <kriss200720020@gmail.com>
2026-05-17 16:42:39 +0500

    main

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
 async def main():
-    print("Бот успешно запущен и готов к работе!")
-    await dp.start_polling(bot)
+    # Твой напарник чуть позже напишет хэндлеры (кнопки, команды)
+    # и мы их подключим (зарегистрируем) прямо здесь:
+    # from handlers import client
+    # dp.include_router(client.router)
 
+    print("working")
+    await dp.start_polling(bot)
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Бот остановлен!")
